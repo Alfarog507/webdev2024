81429f954ce81274c1be676d16c04f9f9784cd93
Gabo502 <alfarog507@gmail.com>
2024-03-09 13:16:36 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,128 @@
+//Express Setup
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const app = express();
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressErrors');
+const JOI = require('joi');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const Review = require('./models/review');
+
+const Campground = require('./models/campgrounds');
+
+//JOI Validation
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+//Mongoose Setup
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+    .then(() => {
+        console.log('Mongo connection open!')
+    })
+    .catch(err => {
+        console.log('Oh no, Mongo connection error!')
+        console.log(err)
+    })
+
+//Server Setup
+app.listen(3000, () => {
+    console.log('Serving on port 3000');
+});
+
+//EJS Setup
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/campgrounds', catchAsync( async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+}));
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+app.get('/campgrounds/:id', catchAsync( async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id).populate('reviews');
+    res.render('campgrounds/show', { campground });
+}));
+
+app.get('/campgrounds/:id/edit', catchAsync( async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', { campground });
+}));
+
+app.put('/campgrounds/:id', validateCampground, catchAsync( async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+app.delete('/campgrounds/:id', catchAsync( async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+}));
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync( async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync( async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh no, something went wrong!';
+    res.status(statusCode).render('error', { err });
+});
---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campgrounds');
+
+//Mongoose Setup
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+    .then(() => {
+        console.log('Mongo connection open!')
+    })
+    .catch(err => {
+        console.log('Oh no, Mongo connection error!')
+        console.log(err)
+    })
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for(let i = 0; i < 50; i++){
+        const rand1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground ({
+            location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in semper nunc. Sed auctor, libero sit amet vulputate fermentum, ligula turpis facilisis est, at tristique enim odio nec dolor. Nulla facilisi.',
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB()
+    .then(() => {
+        mongoose.connection.close();
+    })
