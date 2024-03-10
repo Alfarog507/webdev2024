dddccaccec20afc4178b540e4ff9ae247c81151c
Gabo502 <alfarog507@gmail.com>
2024-03-10 17:36:04 -0600

    finishing the flash course & fixing the review is not defined error

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-//Express Setup
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const app = express();
-const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
-const ExpressError = require('./utils/ExpressErrors');
 const session = require('express-session');
-// const JOI = require('joi');
-// const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressErrors');
+const methodOverride = require('method-override');
+
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -23,24 +21,22 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp')
         console.log(err)
     })
 
-//Server Setup
-app.listen(3000, () => {
-    console.log('Serving on port 3000');
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
 });
 
-//EJS Setup
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
+const app = express();
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, 'public')))
 
-//Routes
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
-app.use(express.static(path.join(__dirname, 'public')));
-
-//Session Setup
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
     resave: false,
@@ -51,18 +47,33 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-app.use(session(sessionConfig));
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home')
 });
 
+
 app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404));
-});
+    next(new ExpressError('Page Not Found', 404))
+})
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh no, something went wrong!';
-    res.status(statusCode).render('error', { err });
-});
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
