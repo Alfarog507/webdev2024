eaab0e8bc9ba51019124e3dbee85155d2d4dbafe
Gabo502 <alfarog507@gmail.com>
2024-03-14 14:43:42 -0600

    finishing the login

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -27,7 +27,6 @@ router.get('/new', isLoggedIn, (req, res) => {
 });
 
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     req.flash('success', 'Successfully made a new campground!');
